b618f16ec2d33e528440982058953a3b0fdefe27
giusttEscolar <111124745+giusttEscolar@users.noreply.github.com>
2022-08-12 09:08:30 -0300

    Add files via upload

---
+++ EX_EditorBase_3J_13/EX_EditorBase_3J_13/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EX_EditorBase_3J_13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exibirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
